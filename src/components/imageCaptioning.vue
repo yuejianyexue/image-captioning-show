70c4314fca8ebe03037160581db7d3145bad6029
yuejianyexue <elpsycongroo919@163.com>
2024-01-30 18:39:36 +0800

    完成数据库交互

--- src/components/imageCaptioning.vue
+++ src/components/imageCaptioning.vue
@@ -2,17 +2,23 @@
     <div>
         <input type="file" accept="image/*" @change="upFile">
          <button @click="show">预览文件</button>
-         <img v-if="imageUrl" :src="imageUrl" />
+         <input type="submit" @click="uploadFile">
+         <div ref="re"></div>
+         <div>
+            <hr>
+             <img class="show" v-if="imageUrl" :src="imageUrl" />
+         </div>
          <!-- <button @click="uploadFile" :disabled="!upFile">上传文件</button> -->
     </div>
 </template>
 
 <script lang='ts' setup>
-// import axios from 'axios';
+import axios from 'axios';
 import {ref} from 'vue';
 
 const imageUrl =ref('')
 const file = ref()
+
 
 //读取用户上传的文件保存到file
 function upFile(event: any) {
@@ -30,6 +36,29 @@ function show(){
     };
     
 } 
+const re = ref()
+function uploadFile(){
+      const formData = new window.FormData()
+      
+      formData.append('file',file.value)
+    console.log(formData.get('file'))
+    axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        re.value.innerHTML = res.data
+      }).catch((err) => {
+        console.log(err);
+      });
+}   
 </script>
 
-<style scoped></style>
+<style scoped>
+.show{
+    max-height: 500px;
+    max-width: 500px;
+}
+</style>
